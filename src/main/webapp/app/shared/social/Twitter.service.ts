7271c85a040cab037bca3bdf1d4e4836e48e7bb0
ahmed <ahmed@ahmed-PC>
2019-10-30 11:03:25 +0200

    adding social

---
+++ src/main/webapp/app/shared/social/Twitter.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SERVER_API_URL } from '../../app.constants';
+import { JhiDateUtils } from 'ng-jhipster';
+import { DatePipe } from '@angular/common';
+import { Facebook } from '../model/Facebook.model';
+import { ISocial } from './ISocial';
+@Injectable({ providedIn: 'root' })
+export class TwitterService implements ISocial {
+  resourceUrl = SERVER_API_URL + 'services/publishbroadcast/api/social';
+
+  constructor(private http: HttpClient, private dateUtils: JhiDateUtils, private datePipe: DatePipe) {}
+
+  FBLongLiveToken(shortToken) {
+    return this.http.get<Facebook>(
+      // tslint:disable-next-line:max-line-length
+      'https://graph.facebook.com/oauth/access_token?%20grant_type=fb_exchange_token&%20client_id=531160574073603&%20client_secret=4b011aa8d93972383d7157689af67a04&%20fb_exchange_token=' +
+        shortToken
+    );
+  }
+  share(content: any) {
+    return this.http.post(`${this.resourceUrl}/facebook/share`, content, { observe: 'response' });
+  }
+  loadComments(broadcast) {
+    return this.http.post(`${this.resourceUrl}/facebook/comments`, broadcast, { observe: 'response' });
+  }
+}
